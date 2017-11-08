57ad444963d730b522996e1dcb919704c23cd176
Adrian Zięba <adrian.zieba@infakt.pl>
2017-11-08 09:03:19 +0100

    remove old specs

--- spec/routing/home_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-
-require "rails_helper"
-
-RSpec.describe 'routes to the home controller', type: :routing do
-  describe 'routing' do
-    it { expect(get: '/').to route_to('home#index') }
-  end
-end
